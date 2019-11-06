ed6d474de91ddfd0db7a8d67c883cb919b2bf0dc
Denny Temple <dev@dentemple.com>
2019-11-05 22:23:58 -0500

    Configure an additional env variable for mock data

--- familiar-mythic-arena/rankings-client/src/metadata.js
+++ familiar-mythic-arena/rankings-client/src/metadata.js
@@ -1,6 +1,8 @@
 export const metadata = {
   appName: 'myFamiliar',
-  productName: 'Familiar: Mythic Arena'
+  productName: 'Familiar: Mythic Arena',
+  env: process.env.NODE_ENV,
+  useMockData: !!process.env.REACT_APP_USE_MOCK_DATA
 }
 
 export default metadata
--- familiar-mythic-arena/rankings-client/src/state/reducers.js
+++ familiar-mythic-arena/rankings-client/src/state/reducers.js
@@ -5,8 +5,8 @@ import metadata from './metadata'
 import testDispatch from './testDispatch'
 
 const reducers = combineReducers({
-  account,
   metadata,
+  account,
   test: testDispatch
 })
 
