e5c420f61a5e8fa996585beef8b0232a9e77f54a
Denny Temple <dev@dentemple.com>
2019-11-09 18:07:40 -0500

    Begin designing front-end
    Begin designing front-end

--- familiar/rankings-client/src/App/index.jsx
+++ familiar/rankings-client/src/App/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
 import Layout from './Layout'
 import Routes from './Routes'
 
-export const App = () => (
-  <Layout>
-    <Routes />
-  </Layout>
-)
+import { fetchProfile } from 'state/profile'
+import { noStateToMap } from 'utils'
 
-export default App
+export class App extends Component {
+  static propTypes = {
+    fetchProfile: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    const { fetchProfile } = this.props
+
+    fetchProfile()
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Routes />
+      </Layout>
+    )
+  }
+}
+
+const mapDispatch = dispatch => ({
+  fetchProfile: () => dispatch(fetchProfile())
+})
+
+export default connect(
+  noStateToMap,
+  mapDispatch
+)(App)
