53bd643865d9939c96f0b283b935cd4e49263a70
Denny Temple <dev@dentemple.com>
2019-11-09 16:10:39 -0500

    WIP

--- familiar-mythic-arena/rankings-client/src/components/screens/Dashboard/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Dasbhoard = () => (
-  <div>
-    <p>Dasbhoard</p>
-  </div>
-)
-
-export default Dasbhoard
--- familiar-mythic-arena/rankings-client/src/components/screens/Page404/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Page404 = () => (
-  <div>
-    <p>Page404</p>
-  </div>
-)
-
-export default Page404
